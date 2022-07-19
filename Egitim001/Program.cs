65086b2cc821de5afb9a91679b6be52546a986ae
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2022-07-19 12:46:54 +0300

    Add files via upload

---
+++ Egitim001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary001;
+
+namespace Egitim001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var ao = new ClassLibrary001.ArkenomOps();
+                var result = ao.CreateMusteriRecord(null, "bicici", "654849");
+
+                if(result.hata)
+                {
+                    Console.WriteLine("ıslem başarılı");
+                }
+                else
+                {
+                    Console.WriteLine(result.hataDetayi);
+                }
+            }
+            catch(Exception ex)
+            {
+                Concole.Writline("101: " + ex.Message); 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
